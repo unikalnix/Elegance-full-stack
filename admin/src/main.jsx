0c1eb6a0df2ddc014afe47343d1e4cc74e984cb0
Malik <hafizdaniyalshakeel@gmail.com>
2025-04-20 12:52:41 +0500

    a

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "../../frontend/src/context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>
 );
