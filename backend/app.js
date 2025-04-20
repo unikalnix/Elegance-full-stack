5752c10f4a824a6e0a0752af24e8049483acfb25
Malik <hafizdaniyalshakeel@gmail.com>
2025-04-20 12:25:38 +0500

    a

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/check-auth", (req, res) => {
-  return res.json({ success: true, message: req.cookies });
+  return res.json({ success: true, message: req.cookies.user_auth_token  });
   // const token = req.cookies.user_auth_token;
   // console.log("Cookies received:", req.cookies); // Debug log
   // console.log("Token:", token); // Debug log
--- backend/controllers/authController.js
+++
@@ -1,7 +0,0 @@
-res.cookie('user_auth_token', token, {
-  httpOnly: true,
-  secure: true,  // Required for HTTPS
-  sameSite: 'none',  // Required for cross-origin cookies
-  path: '/',
-  maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-});
